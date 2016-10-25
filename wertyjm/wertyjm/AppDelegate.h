56501045793b872d564dd70c6b4350905a2fcfc2
wang577250817 <577259817@qq.com>
2016-10-25 09:19:24 +0800

    textOne

---
+++ wertyjm/wertyjm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wertyjm
+//
+//  Created by 王 世江 on 16/10/24.
+//  Copyright © 2016年 王 世江. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wertyjm/wertyjm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wertyjm
+//
+//  Created by 王 世江 on 16/10/24.
+//  Copyright © 2016年 王 世江. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
